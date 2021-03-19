9f9ef5862d54549f4741c11de0746ad38ebf2a6e
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2021-03-19 10:31:29 +0530

    First commit

---
+++ app/src/main/java/com/bphc/dbs_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bphc.dbs_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
